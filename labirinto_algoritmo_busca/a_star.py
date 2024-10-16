535eae72226449c7661081e56cd9819a17042422
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-16 11:42:22 -0300

    Finalizado o algoritmo A* para percorrer o labirinto

--- labirinto_algoritmo_busca/a_star.py
+++ labirinto_algoritmo_busca/a_star.py
@@ -1,7 +1,7 @@
-from pyamaze import maze, agent
+from pyamaze import maze, agent, COLOR
 from queue import PriorityQueue
 
-destino = (30, 30)
+destino = (1, 1)
 
 def h_score(celula, destino):
     linha_atual = celula[0]
@@ -13,25 +13,30 @@ def h_score(celula, destino):
 def a_star(labirinto):
     # Criar um tabuleiro com f_score infinito
     f_score = {celula: float("inf") for celula in labirinto.grid}
+    # Criar Dicionário g_score
     g_score = {}
-    celula_inicial = (labirinto.rows, labirinto.cols)
     # Calcular o valor da célula inicial
+    celula_inicial = (labirinto.rows, labirinto.cols)
     g_score[celula_inicial] = 0
     f_score[celula_inicial] = g_score[celula_inicial] + h_score(celula_inicial, destino)
-    print(f_score)
-
+    # Definir fima de prioridade para controle dos caminhos
     fila = PriorityQueue()
     item = (f_score[celula_inicial], h_score(celula_inicial, destino), celula_inicial)
     fila.put(item)
 
+    caminho = {}
+    # Percorrer caminho utilizando a fila
     while not fila.empty():
-        celula = fila.get()
+        celula = fila.get()[2]
+
         if celula == destino:
             break
+
         for direcao in "NSEW":
             if labirinto.maze_map[celula][direcao] == 1:
                 linha_celula = celula[0]
                 coluna_celula = celula[1]
+                # Calcular qual a proxima célula
                 if direcao == "N":
                     proxima_celula = (linha_celula - 1, coluna_celula)
                 elif direcao == "S":
@@ -40,32 +45,30 @@ def a_star(labirinto):
                     proxima_celula = (linha_celula, coluna_celula - 1)
                 elif direcao == "E":
                     proxima_celula = (linha_celula, coluna_celula + 1)
+                # Calcular g_score e f_core da proxima celua
                 novo_g_score = g_score[celula] + 1
                 novo_f_score = novo_g_score + h_score(proxima_celula, destino)
-
-                if novo_g_score < f_score[proxima_celula]:
+                #Verificar se o caminho terá um menor custo
+                if novo_f_score < f_score[proxima_celula]:
                     f_score[proxima_celula] = novo_f_score
                     g_score[proxima_celula] = novo_g_score
                     item = (novo_f_score, h_score(proxima_celula, destino), proxima_celula)
                     fila.put(item)
-
-    # Caminhar a partir da celula inicial, explorando os proximos caminhos
-        # Para cada possibilidade de caminho
-            # Se o caminho é possivel (sem parede)
-            # Se o f_score calculado < f_score do caminho antigo
-                # Substituir o f_score antigo pelo calculado
-                # Escolher o caminho para seguir que tem:
-                    # O menor f_score
-                    # Se os f_scores forem iguais, o que tem menor h_score
-    caminho = ""
-    return caminho
+                    caminho[proxima_celula] = celula
+    # Caminho da rota perfeita
+    caminho_final = {}
+    celula_analisada = destino
+    while celula_analisada != celula_inicial:
+        caminho_final[caminho[celula_analisada]] = celula_analisada  # invertendo chave e valor do dicionário caminho
+        celula_analisada = caminho[celula_analisada]  # atualizando para a próxima célula analisada
+    return caminho_final
 
 
-labirinto = maze(20,20)
+labirinto = maze(30,30)
 
-labirinto.CreateMaze(loadMaze="maze--2024-10-16--09-58-35.csv")
+labirinto.CreateMaze()
 
 caminho = a_star(labirinto)
-agente = agent(labirinto, filled=True, footprints=True)
-labirinto.tracePath({agente:caminho}, delay=150)
+agente = agent(labirinto, filled=True, footprints=True, color=COLOR.cyan)
+labirinto.tracePath({agente:caminho}, delay=50)
 labirinto.run()
